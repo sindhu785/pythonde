0c4d3ba91f43f12bd0f249b0d797f2d6fed24335
unknown <190031785@kluniversity.in>
2021-08-31 23:55:39 +0530

    first commit

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum{
+ public static void main(String[] args){
+	System.out.println("Enter two numbers");
+	int a=10;
+	int b=20;
+	System.out.println(a+" "+b);
+	int sum=a+b;
+	System.out.println(sum);
+}
+}
